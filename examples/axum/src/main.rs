b8902aff8a9a8b78f83a1c5174c63e083f08768e
snapiz <jonathan.lapiquonne@gmail.com>
2023-05-27 20:50:05 +0200

    feat: axum integration

---
+++ examples/axum/src/main.rs
@@ -0,0 +1,28 @@
+use axum::{response::IntoResponse, routing::get, Json, Router};
+use serde::Deserialize;
+use twa_jwks::{axum::JwtPayload, JwksClient};
+
+#[derive(Deserialize)]
+struct JwtClaims {
+    pub sub: String,
+}
+
+async fn hello(JwtPayload(payload): JwtPayload<JwtClaims>) -> impl IntoResponse {
+    Json(format!("hello from axum, {}", payload.sub))
+}
+
+#[tokio::main]
+async fn main() {
+    let jwks_client = JwksClient::build(Some("http://127.0.0.1:6550/.well-known/jwks.json"))
+        .await
+        .unwrap();
+
+    let app = Router::new()
+        .route("/hello", get(hello))
+        .with_state(jwks_client);
+
+    axum::Server::bind(&([127, 0, 0, 1], 3001).into())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
--- twa-jwks/src/lib.rs
+++ twa-jwks/src/lib.rs
@@ -14,6 +14,9 @@ pub mod keyset;
 #[cfg(feature = "actix-web")]
 pub mod actix_web;
 
+#[cfg(feature = "axum")]
+pub mod axum;
+
 #[derive(ThisError, Debug)]
 pub enum Error {
     #[error("reqwest: {0}")]
